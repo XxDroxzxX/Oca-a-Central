431932cc79627351dea65963062eb6cf8422d583
XxDroxzxX <ande.mau100@gmail.com>
2024-05-26 17:18:10 -0500

    Modificacion de estilos

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2024 OcañaCentral. All Rights reserved.</p>
       </div>
     </footer>
   )
